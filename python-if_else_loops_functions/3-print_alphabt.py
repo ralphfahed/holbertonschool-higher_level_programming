90db549b075b489e0ec6ab52275a7ddc4adf359a
ralph <ralphh.fahed@gmail.com>
2025-03-18 14:08:51 +0000

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 # Using only one print function and string format
-print("".join("{}".format(chr(x)) for x in range(97, 123) if chr(x) != 'q' and chr(x) != 'e'), end="")
+print("".join("{}".format(chr(x)) 
+      for x in range(97, 123) 
+      if chr(x) != 'q' and chr(x) != 'e'), end="")
