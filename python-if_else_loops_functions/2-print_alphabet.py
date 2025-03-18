c1f521b2c5f780d97a2226c12f12288ac71e3d67
ralph <ralphh.fahed@gmail.com>
2025-03-18 13:55:22 +0000

    task 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-# Use the join method to create a single string with all the lowercase letters
-print("".join(chr(x) for x in range(97, 123)), end="")
+# Using only one print function and string format
+print("".join("{}".format(chr(x)) for x in range(97, 123)), end="")
