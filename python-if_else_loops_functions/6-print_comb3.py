2bc5a8538141aa7bc6325ea782bdc69590c38382
ralph <ralphh.fahed@gmail.com>
2025-03-19 16:32:24 +0000

    task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+for i in range(10):            # First digit from 0 to 9
+    for j in range(i + 1, 10): # Second digit must be greater than first
+        if i == 8 and j == 9:  # Last combination (no comma)
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
