260e0f8a4ab4f26d2a40b15eaed6f450a4e9e2bd
ralph <ralphh.fahed@gmail.com>
2025-03-20 09:28:14 +0000

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for i in range(1, 101):  # Range should be from 1 to 100 (inclusive)
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
@@ -10,4 +10,3 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
-fizzbuzz()
