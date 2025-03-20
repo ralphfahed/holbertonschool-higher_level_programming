e04f3df0644b3d6b94490d3de10b80051940b0eb
ralph <ralphh.fahed@gmail.com>
2025-03-20 19:51:55 +0000

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 import sys
 
 if __name__ == "__main__":
-    numbers = [int(arg) for arg in sys.argv[1:]]  # Convert arguments to integers
-    total = sum(numbers)  # Sum up the integers
+    numbers = [int(arg) for arg in sys.argv[1:]]
+    total = sum(numbers)
     print(total)
