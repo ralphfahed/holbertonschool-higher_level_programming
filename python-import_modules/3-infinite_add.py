aeaa5385688101c40f41c05c04c6e0118cef1c11
ralph <ralphh.fahed@gmail.com>
2025-03-20 20:03:21 +0000

    task 5

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import sys
-
+'''
+mna3mol var esma numbers = array ta nhot bi alba []
+men 7awel arg la int => int(arg)
+for loop mnobrom bel sys.arg[1:] men doun esem lfile
+sum () is a built in function 
+'''
 if __name__ == "__main__":
     numbers = [int(arg) for arg in sys.argv[1:]]
     total = sum(numbers)
