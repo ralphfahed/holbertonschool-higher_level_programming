67a0896d92c8ea5cde5fd40f80d67992904a19f9
ralph <ralphh.fahed@gmail.com>
2025-03-20 08:52:14 +0000

    task 10

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
+    #abs ta testa2bel the negative numbers
+    #ba3mol %10 ta jib ekher digit
     last_digit = abs(number) % 10
     print(last_digit, end="")
     return last_digit
