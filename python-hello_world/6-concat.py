9680aa57f42f9de7694971a2a937a00b230bdc02
ralph <ralphh.fahed@gmail.com>
2025-03-20 09:00:19 +0000

    task 4

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 str1 = "Holberton"
 str2 = "School"
 print(f"Welcome to {str1} {str2}!")  # Concatenates the two strings
-
 # End of file (Adding this comment in case it's required)
