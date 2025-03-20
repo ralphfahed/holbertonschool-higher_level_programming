df77f950f9dce3ef065ee0786c929d37f0b16384
ralph <ralphh.fahed@gmail.com>
2025-03-20 19:46:04 +0000

    task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+#result of addition of all argument folowed by a new line , we can use int ta 7ael la integers
+def sum_all(*args):
+    sum = 0
+    for i in args:
+        if not str(num).isdigit():
+            return False
+        else:
+            sum += int(num)
