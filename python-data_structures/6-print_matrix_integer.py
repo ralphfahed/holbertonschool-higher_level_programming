0efa6c728b637e7e4a5a7c08934f478ff160f85e
ralph <ralphh.fahed@gmail.com>
2025-03-24 10:18:18 +0000

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:  # Loop through each row
-        for i, element in enumerate(row):  # Use enumerate to track the index
-            if i != len(row) - 1:  # If it's not the last element in the row
-                print("{:d}".format(element), end=" ")  # Print element with space
-            else:
-                print("{:d}".format(element))  # Print last element without space
+    if not matrix or all(not row for row in matrix):  # Check if matrix is empty or all rows are empty
+        print()  # Print a blank line if the matrix is empty or all rows are empty
+    else:
+        for row in matrix:  # Loop through each row
+            for i, element in enumerate(row):  # Use enumerate to track the index
+                if i != len(row) - 1:  # If it's not the last element in the row
+                    print("{:d}".format(element), end=" ")  # Print element with space
+                else:
+                    print("{:d}".format(element))  # Print last element without space
