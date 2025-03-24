6847eda05e6f4d2714754bac7805b41dafd8f39b
ralph <ralphh.fahed@gmail.com>
2025-03-24 11:09:18 +0000

    11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        # Create a new list without the element at the specified index using slicing
+        my_list = my_list[:idx] + my_list[idx+1:]
+        return my_list
+
