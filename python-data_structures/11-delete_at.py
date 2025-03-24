0aa3cf9ba3c983e084570b926088bbaf0160d42d
ralph <ralphh.fahed@gmail.com>
2025-03-24 11:17:41 +0000

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    # Check if the index is out of range (negative or greater than the list length)
+    # Check if the index is negative or out of range
     if idx < 0 or idx >= len(my_list):
-        return my_list  # Return the list unchanged if index is invalid
-    else:
-        # Remove the element at the given index using slicing
-        my_list = my_list[:idx] + my_list[idx+1:]
         return my_list
+    new_list = my_list[:idx] + my_list[idx+1:]
+    return new_list
+
