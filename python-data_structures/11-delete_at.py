3e1ff974b9523acda965e8cb555dcf34bdc289a4
ralph <ralphh.fahed@gmail.com>
2025-03-24 11:58:28 +0000

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=None, idx=0):
-    # Initialize my_list to an empty list if it is None
-    if my_list is None:
-        my_list = []
-    
-    # Check if the index is negative or out of range
+def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-
-    new_list = my_list[:idx] + my_list[idx+1:]
-    return new_list
+    return my_list[:idx] + my_list[idx+1:]
