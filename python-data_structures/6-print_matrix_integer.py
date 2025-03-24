828fbcce9ced9fbd79ba694030d1acee940b844f
ralph <ralphh.fahed@gmail.com>
2025-03-24 10:13:58 +0000

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# marix ya3ne bet2asema 3 elements w 3 rows chi hek
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:  # loop bi 2aleb kel row
+        for element in row:  # loop bel elemetns li bel row
+            print(element, end=" ")  # Print the element with a space
+        print()  # Print a new line after each row
