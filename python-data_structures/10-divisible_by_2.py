f660af8b917886ea287e31dec2eb48d3f8786168
ralph <ralphh.fahed@gmail.com>
2025-03-24 11:47:06 +0000

    task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if my_list == []:
+        return []
+    
+    result = []
+    for i in my_list:
+        if i % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+    return result
