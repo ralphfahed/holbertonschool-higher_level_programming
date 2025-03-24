461a666af7dcbad2ba5ba45d78f0a31e21cd7d57
ralph <ralphh.fahed@gmail.com>
2025-03-24 09:56:11 +0000

    5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     new_string = ""  # create a new empty string
     for i in my_string:  # loop in the string
         if i != 'c' and i != 'C':
-            new_string += i  # Add the character to the new string if it's not 'c' or 'C'
+            new_string += i  # Add the character to the new string
     return new_string  # Return the new string after the loop
