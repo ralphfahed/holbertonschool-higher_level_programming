4cebc334048ecddfd3ee63589e138a1afe6614ec
ralph <ralphh.fahed@gmail.com>
2025-03-25 15:17:03 +0000

    task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-def print_sorted_list_of_dictionaries(list_of_dicts):
-    for a_dictionary in list_of_dicts:
-        for key in sorted(a_dictionary.keys()):
-            print(f"{key}: {a_dictionary[key]}")
-# for a_dictionary in list_of_dicts: This loops over each dictionary in list.
-# for key in sorted(a_dictionary.keys()):  we sort its keys in ascending order
-# print(f"{key}: {a_dictionary[key]}"): sorted key, we print the key and value
+def print_sorted_dictionary(a_dictionary):
+    # Sort the dictionary keys and print the key-value pairs
+    for key in sorted(a_dictionary.keys()):  # Sorts the dictionary keys
+        print(f"{key}: {a_dictionary[key]}")  # Prints each key-value pair in sorted order
