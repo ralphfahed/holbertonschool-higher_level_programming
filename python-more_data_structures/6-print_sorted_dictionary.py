cd44ce0f80622a769de04e2b8976bcb86f52c16b
ralph <ralphh.fahed@gmail.com>
2025-03-25 15:17:53 +0000

    task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@
 def print_sorted_dictionary(a_dictionary):
     # Sort the dictionary keys and print the key-value pairs
     for key in sorted(a_dictionary.keys()):  # Sorts the dictionary keys
-        print(f"{key}: {a_dictionary[key]}")  # Prints each key-value pair in sorted order
+        print(f"{key}: {a_dictionary[key]}")  # Prints each key : value
