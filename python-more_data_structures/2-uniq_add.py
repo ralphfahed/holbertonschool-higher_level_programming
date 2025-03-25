072303e13db0424a3e74dcc5c18e9ba3a335bea3
ralph <ralphh.fahed@gmail.com>
2025-03-25 14:44:47 +0000

    task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,9 +2,9 @@
 
 def uniq_add(my_list=[]):
     unique_values = []  # New list to track unique values
-    result = 0  # To store the sum 
+    result = 0  # To store the sum
     for i in my_list:
         if i not in unique_values:  # If i is not already in the newlist
-            unique_values.append(i)  # Add 'i' to the  newlist
+            unique_values.append(i)  # Add 'i' to the newlist
             result += i  # Add 'i' to the total sum
     return result
