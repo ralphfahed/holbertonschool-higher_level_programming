9777baa29dc13e7566fe997caa8147b0393436c6
ralph <ralphh.fahed@gmail.com>
2025-03-25 14:29:51 +0000

    task 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)  # Add 'replace' instead of 'search'
+        else:
+            new_list.append(item)  #  Keep the original value
+    return new_list  # Return the new list
