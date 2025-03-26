03e4e2e457ebba950ccbe69ab2979239de6eb01e
ralph <ralphh.fahed@gmail.com>
2025-03-26 09:10:43 +0000

    task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value), end='')
-        return 1
+        return True
     except (ValueError, TypeEror):
-        return 0
+        return False
