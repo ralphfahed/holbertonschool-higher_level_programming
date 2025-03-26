362ce59391983007f3c6dcba54926c952141387e
ralph <ralphh.fahed@gmail.com>
2025-03-26 10:36:14 +0000

    6

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    raise TypeError(message)  # Raise a TypeError with a custom message
+    raise NameError(message)  # Raise a NameError with a custom message
