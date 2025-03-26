b61f1e1609a721157bea07592dde599a977ba9cc
ralph <ralphh.fahed@gmail.com>
2025-03-26 09:21:17 +0000

    task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,10 +2,8 @@
 
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value), end='')
+        print("{:d}".format(value))
         return True
     except (ValueError, TypeEror):
         print("{} is not an integer".format(value))
         return False
-    finaly:
-        print("")
