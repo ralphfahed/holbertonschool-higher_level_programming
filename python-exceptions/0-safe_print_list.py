78979994c7f7332a3ed86beef4edf9de956c67c5
ralph <ralphh.fahed@gmail.com>
2025-03-26 08:52:42 +0000

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0  # Initialize the count variable
     try:
         for i in range(x):
-            print(my_list[i], end=' ')  # Print element followed by a space
+            print(my_list[i])
             count += 1  # Increment the count for each printed element
     except IndexError:
         pass  # If the index exceeds the list length, just ignore the error
