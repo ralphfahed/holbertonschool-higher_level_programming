039030f654649668c0c41f44d17860d6005c800d
ralph <ralphh.fahed@gmail.com>
2025-03-26 10:38:47 +0000

    1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeEror):
-        print("{} is not an integer".format(value))
+    except (ValueError, TypeError):
         return False
