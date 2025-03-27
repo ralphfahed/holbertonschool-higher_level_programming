234de3d210ee8ef982e4f89291cec53560b791c3
ralph <ralphh.fahed@gmail.com>
2025-03-27 11:02:06 +0000

    test 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -21,33 +21,40 @@ def max_integer(list=[]):
 class TestMaxInteger(unittest.TestCase):
     """Test cases for the max_integer function"""
 
-    def test_max_integer(self):
-        """Test all cases for max_integer function"""
-        # Test empty list
+    def test_list_is_empty(self):
+        """Test empty list"""
         self.assertIsNone(max_integer([]))
 
-        # Test list with one element
+    def test_list_with_one_element(self):
+        """Test list with one element"""
         self.assertEqual(max_integer([98]), 98)
 
-        # Test max at the end
+    def test_max_at_the_end(self):
+        """Test max at the end"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-        # Test max at the beginning
+    def test_max_at_the_beginning(self):
+        """Test max at the beginning"""
         self.assertEqual(max_integer([4, 1, 2, 3]), 4)
 
-        # Test max in the middle
+    def test_max_in_the_middle(self):
+        """Test max in the middle"""
         self.assertEqual(max_integer([1, 2, 4, 3]), 4)
 
-        # Test one negative number in the list
+    def test_one_negative_number_in_the_list(self):
+        """Test one negative number in the list"""
         self.assertEqual(max_integer([1, -1, 2, 3]), 3)
 
-        # Test only negative numbers in the list
+    def test_only_negative_numbers_in_the_list(self):
+        """Test only negative numbers in the list"""
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-        # Test list with identical values
+    def test_list_with_identical_values(self):
+        """Test list with identical values"""
         self.assertEqual(max_integer([4, 4, 4, 4]), 4)
 
-        # Test list with one float
+    def test_list_with_one_float(self):
+        """Test list with one float"""
         self.assertEqual(max_integer([1, 2, 3.5, 2]), 3.5)
 
 if __name__ == "__main__":
