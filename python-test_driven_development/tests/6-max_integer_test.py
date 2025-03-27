c37650d508a52725c3aef0de5621c0bb0ec25d18
ralph <ralphh.fahed@gmail.com>
2025-03-27 10:41:01 +0000

    task 6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import unittest
+
+"""Module to find the max integer in a list
+"""
+
+
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
+
+class SimpleTest(unittest.Testcase):
+    def testmax(self):
+        self.assertEqual(max_integer([1,2,3,4]),4)
+        self.assertEqual(max_integer([1,3,4,2]),4)
+
+if __name__ = "__main__":
+    unittest.main(verbosity=2)
