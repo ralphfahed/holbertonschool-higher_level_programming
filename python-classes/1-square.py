968fc874156f4e639071066f16f65db6189e2a90
ralph <ralphh.fahed@gmail.com>
2025-04-01 13:28:39 +0000

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,4 +5,9 @@
 class Square:
     """An class that defines the size of a square."""
     def __init__(self, size):
-        self.__size = size:
+        """Initialize a new Square.
+        
+        Args:
+            size: The size of the new square.
+        """
+        self.__size = size
