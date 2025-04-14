f3513f1f0d9ac180145c0c0f49f86f66d6729cb3
ralph <ralphh.fahed@gmail.com>
2025-04-14 10:13:45 +0000

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a list and saves them to a JSON file."""
+"""Script that adds all arguments to a Python list and saves them in a JSON file."""
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
-def main():
-    """Main function that adds arguments to a list and saves to a JSON file"""
-    # Get all arguments passed (excluding the script name itself)
-    arguments = sys.argv[1:]
+filename = "add_item.json"
 
-    try:
-        # Try to load the existing list from the JSON file
-        current_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        # If file doesn't exist, start with an empty list
-        current_list = []
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
 
-    # Add the new arguments to the list
-    current_list.extend(arguments)
-
-    # Save the updated list back to the JSON file
-    save_to_json_file(current_list, "add_item.json")
-
-if __name__ == "__main__":
-    main()
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
