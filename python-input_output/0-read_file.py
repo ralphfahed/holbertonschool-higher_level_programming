7faac07649acf18c9a9480a64a4511300f69420e
ralph <ralphh.fahed@gmail.com>
2025-04-14 08:20:41 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 """Module to read and print a UTF-8 text file to stdout."""
 
 #!/usr/bin/python3
-
+# Function that reads a UTF-8 text file and prints its contents to stdout
 
 def read_file(filename=""):
     """Reads a text file (UTF-8) and prints its contents to stdout."""
