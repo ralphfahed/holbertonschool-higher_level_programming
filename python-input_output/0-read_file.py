7a5d3c957ddaf1ae301e0184d9d664684730c2a3
ralph <ralphh.fahed@gmail.com>
2025-04-14 08:13:35 +0000

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Function that reads a UTF-8 text file and prints its contents to stdout
+
+def read_file(filename=""):
+    # Use 'with' to open the file safely and ensure it closes automatically
+    with open(filename, encoding="utf-8") as f:
+        # Read the entire file content and print it without adding extra newline
+        print(f.read())
---
+++ python-input_output/README.md
@@ -0,0 +1,1 @@
+open files closed file
