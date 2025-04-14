5c55bab4c493283f5493237410827a98633aee0f
ralph <ralphh.fahed@gmail.com>
2025-04-14 11:20:49 +0000

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and saves them to a file"""
+"""
+Script that adds all arguments to a Python list and saves them to a file.
+
+This script uses save_to_json_file and load_from_json_file functions to
+manage a list of arguments in a JSON file. It loads existing data if present,
+adds new command line arguments, and saves the updated list back to the file.
+"""
 import sys
 from os.path import exists
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+try:
+    from 5-save_to_json_file import save_to_json_file
+    from 6-load_from_json_file import load_from_json_file
+except ImportError:
+    from save_to_json_file import save_to_json_file
+    from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
