7d07660001402c801b29ae755904f04e8532ecaf
ralph <ralphh.fahed@gmail.com>
2025-04-14 08:15:39 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 """Module that provides a function to read and print a UTF-8 text file to stdout."""
 
 #!/usr/bin/python3
-# Function that reads a UTF-8 text file and prints its contents to stdout
 
 def read_file(filename=""):
-    # Use 'with' to open the file safely and ensure it closes automatically
+    """Reads a text file (UTF-8) and prints its contents to stdout."""
     with open(filename, encoding="utf-8") as f:
-        # Read the entire file content and print it without adding extra newline
-        print(f.read())
+        print(f.read(), end="")
