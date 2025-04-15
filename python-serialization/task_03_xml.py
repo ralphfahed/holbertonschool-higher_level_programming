51035cd7f0e1fc97d1ffc3c9e684110a56bfeb48
ralph <ralphh.fahed@gmail.com>
2025-04-15 09:28:31 +0000

    3

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,9 +1,18 @@
 import xml.etree.ElementTree as ET
-"""Module for XML serialization and deserialization"""
+
+"""Module for serializing and deserializing Python dictionaries using XML format."""
 
 
 def serialize_to_xml(dictionary, filename):
-    """Serialize a Python dictionary to an XML file"""
+    """Serialize a Python dictionary to an XML file.
+
+    Args:
+        dictionary (dict): The dictionary to serialize.
+        filename (str): The filename to save the XML data.
+
+    Returns:
+        None
+    """
     root = ET.Element("data")
 
     for key, value in dictionary.items():
@@ -12,3 +21,21 @@ def serialize_to_xml(dictionary, filename):
 
     tree = ET.ElementTree(root)
     tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    """Deserialize an XML file to a Python dictionary.
+
+    Args:
+        filename (str): The XML filename to read.
+
+    Returns:
+        dict: The deserialized dictionary, or None if an error occurs.
+    """
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        return {child.tag: child.text for child in root}
+    except Exception:
+        return None
