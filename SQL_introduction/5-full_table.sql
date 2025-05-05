e5ee2614f0c1067652fe06d96917b7e09ff70882
ralph <ralphh.fahed@gmail.com>
2025-05-05 17:41:28 +0000

    5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Script that prints the description of the first_table in the given database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_test_db_5'
-ORDER BY ORDINAL_POSITION;
+SHOW CREATE TABLE first_table;
