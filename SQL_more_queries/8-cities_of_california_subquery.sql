084b77f155eacf5f545bf4211b1859b645203ac4
ralph <ralphh.fahed@gmail.com>
2025-05-08 07:48:24 +0000

    8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- List all cities of California without using JOIN
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id AND states.name = 'California'
+ORDER BY cities.id ASC;
