b2fccc8c721b538da4bb0d4966543f61a5469be1
ralph <ralphh.fahed@gmail.com>
2025-05-08 07:41:43 +0000

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,13 +1,13 @@
 -- Create the database if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Use the created database
+-- Use the database
 USE hbtn_0d_usa;
 
--- Create the table 'cities' if it doesn't already exist
+-- Create the 'cities' table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
     state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
