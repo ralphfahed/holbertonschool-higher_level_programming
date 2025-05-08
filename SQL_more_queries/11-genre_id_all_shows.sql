ea3087be302c254c29e222abda250729793a4cff
ralph <ralphh.fahed@gmail.com>
2025-05-08 08:28:43 +0000

    11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,9 @@
 -- Script that lists all shows and their linked genre_id
 -- Results are sorted by tv_shows.title and tv_show_genres.genre_id
+-- Shows with no linked genre will have NULL for genre_id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_show_genres.show_id = tv_shows.id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
