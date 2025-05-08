72cddb970a070c1bd077e6520b6a56a93c43fb9f
ralph <ralphh.fahed@gmail.com>
2025-05-08 10:49:53 +0000

    task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,11 @@
--- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- Script to list all shows and their linked genres, with NULL if no genre is linked
+
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
+-- Left join with tv_show_genres to get the show-genre relationships
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+-- Left join with tv_genres to get the genre names
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title;
+-- Sorting the results by show title and genre name in ascending order
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
 
