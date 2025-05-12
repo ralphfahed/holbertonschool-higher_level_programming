a29a982c4bdf8c821fdf502e40376da9d842c86b
ralph <ralphh.fahed@gmail.com>
2025-05-12 10:13:29 +0000

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
-    # Connect to MySQL server
-    def list_states():
-    """Connects to the database and lists all states sorted by id ASC"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
     # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
-    # Create cursor and execute query
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # Create cursor object
+    cursor = db.cursor()
+
+    # Execute query to get all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     # Fetch and print results
-    rows = cur.fetchall()
-    for row in rows:
+    results = cursor.fetchall()
+    for row in results:
         print(row)
 
-     # Clean up
-    cur.close()
+    # Close cursor and database connection
+    cursor.close()
     db.close()
