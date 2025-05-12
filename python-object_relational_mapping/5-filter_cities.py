963d0806c659323a1e6090df274f598fdf7d509b
ralph <ralphh.fahed@gmail.com>
2025-05-12 12:19:54 +0000

    6

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa"""
+"""
+Lists all cities of a given state from the database hbtn_0e_4_usa
+Usage: ./5-filter_cities.py <username> <password> <database> <state_name>
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connect to MySQL server
+    # Get command-line arguments
+    username, password, db_name, state_name = sys.argv[1:5]
+
+    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        search_name = argv[4]
+        user=username,
+        passwd=password,
+        db=db_name
     )
 
-    # Create cursor object
+    # Create cursor
     cursor = db.cursor()
+
+    # Execute a parameterized query to prevent SQL injection
     query = """
-        SELECT cities.id, cities.name, states.name
+        SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
         ORDER BY cities.id ASC
     """
-    # Execute query to get all cities with their state names
-    cursor.execute(query, (search_name,))
+    cursor.execute(query, (state_name,))
 
     # Fetch and print results
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
+    rows = cursor.fetchall()
+    city_names = [row[0] for row in rows]
+    print(", ".join(city_names))
 
-    # Close cursor and database connection
+    # Clean up
     cursor.close()
     db.close()
