f3e4e37034c50d2b4830bcaf540bdf65cd87a099
ralph <ralphh.fahed@gmail.com>
2025-06-05 09:42:23 +0000

    2

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+const redHeader = document.getElementById('red_header');
+
+redHeader.addEventListener('click', function () {
+  const header = document.querySelector('header');
+  header.classList.add('red');
+});
