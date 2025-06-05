d5798918d3c96d6e30ac0de8c64ef92925220215
ralph <ralphh.fahed@gmail.com>
2025-06-05 11:07:30 +0000

    8

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      const helloElement = document.getElementById('hello');
+      helloElement.textContent = data.hello;
+    })
+    .catch(error => {
+      console.error('Error fetching greeting:', error);
+    });
+});
