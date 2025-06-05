660ede456f9dd17c4582ccdd4488694981a6801f
ralph <ralphh.fahed@gmail.com>
2025-06-05 10:03:12 +0000

    5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+const updateHeader = document.getElementById('update_header');
+
+updateHeader.addEventListener('click', function () {
+  const header = document.querySelector('header');
+  header.textContent = 'New Header!!!';
+});
